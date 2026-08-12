80da63afb9750d6f8878523a7dc95e888172a24f
David Fra Rios Arboleda <fra@Davids-MacBook-Air.local>
2026-08-12 10:42:11 -0500

    Tutorial Laravel: vista y controlador

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+ 
+namespace App\Http\Controllers;
+
+use Illuminate\View\View;
+ 
+class HomeController extends Controller
+{
+    public function index(): View
+    {
+        return view('home.index');
+    }
+    public function about(): View
+{
+    $data1 = "About us - Online Store";
+    $data2 = "About us";
+    $description = "This is an about page ...";
+    $author = "Developed by: Your Name";
+
+    return view('home.about')->with("title", $data1)
+        ->with("subtitle", $data2)
+        ->with("description", $description)
+        ->with("author", $author);
+}
+    
+}
+
+
